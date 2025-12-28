a117fab7924f6816665457840eaa4f38147ddb3a
Md Syful Khan <mdsyfulkhan9899@gmail.com>
2025-12-28 19:51:12 +0600

    fix some issues

--- src/components/sections/testimonial/TestimonialStyle8.jsx
+++ src/components/sections/testimonial/TestimonialStyle8.jsx
@@ -1,20 +1,7 @@
-x: number): string;
-  export function weekdaysShort(): string[];
-  export function weekdaysShort(index: number): string;
-  export function weekdaysShort(format: string): string[];
-  export function weekdaysShort(format: string, index: number): string;
-  export function weekdaysShort(localeSorted: boolean): string[];
-  export function weekdaysShort(localeSorted: boolean, index: number): string;
-  export function weekdaysShort(localeSorted: boolean, format: string): string[];
-  export function weekdaysShort(localeSorted: boolean, format: string, index: number): string;
-  export function weekdaysMin(): string[];
-  export function weekdaysMin(index: number): string;
-  export function weekdaysMin(format: string): string[];
-  export function weekdaysMin(format: string, index: number): string;
-  export function weekdaysMin(localeSorted: boolean): string[];
-  export function weekdaysMin(localeSorted: boolean, index: number): string;
-  export function weekdaysMin(localeSorted: boolean, format: string): string[];
-  export function weekdaysMin(localeSorted: boolean, format: string, index: number): string;
+import React from "react";
 
-  export function min(moments: Moment[]): Moment;
-  export function min(...moments: Moment[]): Mom
+const TestimonialStyle8 = () => {
+  return <div>TestimonialStyle8</div>;
+};
+
+export default TestimonialStyle8;
